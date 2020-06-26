af7db453c1dfe3f46a099cf90833c2d86f51936a
Jesper Armouti-Hansen <j.hansen@uni.koeln.de>
2020-06-26 22:17:14 +0200

    matplotlib float

--- config.h
+++ config.h
@@ -72,6 +72,7 @@ static const Rule rules[] = {
 	{ "St",       NULL,       NULL,       	    0,            0,           1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	{ "matplotlib",NULL,       NULL,            0,            1,           0,         1,        -1 },
+	{ NULL,       NULL,       "Figure 1",       0,            1,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
 };
